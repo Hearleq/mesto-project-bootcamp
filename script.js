af4e020c61c4108e474b6a47f69dde61ef072231
Valeriy B <hearleq@yandex.ru>
2022-11-24 23:44:52 +0300

    22.11.24 mesto-first-draft

---
+++ script.js
@@ -0,0 +1,7 @@
+const popup = document.querySelector('.popup');
+const title = document.querySelector('.popup__title');
+const editButton = document.querySelector('.edit');
+const nameInput = document.querySelector('.profile__name');
+const activityInput = document.querySelector('.profile__activity');
+const saveButton = document.querySelector('.popup__save-button');
+const closeButton = document.querySelector('.popup__close-button');
